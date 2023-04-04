dc2c29ab048cfab561044cab04cad33d59c14cbe
franfran20 <rickyegboluche@gmail.com>
2023-04-04 01:40:40 -0700

    optimizing for next js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-const nextConfig = {
-  reactStrictMode: true,
-  distDir: "build",
-};
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-module.exports = nextConfig;
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+};
